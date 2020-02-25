0be6b5b037838a7fd896e48cc46451263ca207e4
FinishedManx843 <56133570+FinishedManx843@users.noreply.github.com>
2020-02-25 10:50:31 -0600

    Membean_Bot_V1.1
    Added ability to turn on and off.

---
+++ Membean_Bot_V1.1.js
@@ -0,0 +1,67 @@
+var check;
+var x;
+var time = 5000;
+var stop;
+
+var toggle = document.createElement("BUTTON");
+toggle.id = "toggle";
+toggle.setAttribute('onclick', "start()");
+toggle.style.width = "50px";
+toggle.style.height = "25px";
+toggle.style.border = "3px solid black";
+toggle.style.margin = "auto";
+toggle.innerHTML = "Off";
+toggle.style.background = "white";
+document.body.appendChild(toggle);
+
+function start() {
+	if(document.getElementById("toggle").innerHTML == "Off") {
+		document.getElementById("toggle").innerHTML = "On";
+		set("norm");
+	} else if (document.getElementById("toggle").innerHTML == "On") {
+		document.getElementById("toggle").innerHTML = "Off";
+		set("stop");
+	}
+}
+
+function test() {
+	if(document.Pass) {
+		set("clear");
+		console.log("Pass element exists, clicking");
+		set("click");
+	} else if (document.getElementById("next-btn")) {
+		set("clear");
+		x = document.querySelectorAll("#choice-section li");
+		console.log("Next button");
+		x[0].click();
+		x[1].click();
+		x[2].click();
+		document.getElementById("next-btn").click();
+		set("delay");
+	} else {
+		console.log("No recognized elements");
+	}
+} function click() {
+console.log("clicked");
+	document.Pass.click();
+	set("delay");
+} 
+
+function set(type) {
+	if(type == "norm") {
+	clearInterval(stop);
+	check = setInterval(test, 1);
+	} else if(type == "delay") {
+		setTimeout(function del() {check = setInterval(test, 1);}, 1000);
+	} else if(type == "clear") {
+		clearInterval(check);
+	} else if(type == "click") {
+		setTimeout(click, time);
+	} else if (type == "stop") {
+		stop = setInterval(stop, 1);
+	}
+} function stop() {
+	set("clear");
+}
+
+
