b3e82d1f64ba8e9049c8aee86fcb0e386cb698e3
FinishedManx843 <56133570+FinishedManx843@users.noreply.github.com>
2020-02-27 09:27:54 -0600

    Update Membean_Bot_V2.0.js

--- Membean_Bot_V2.0.js
+++ Membean_Bot_V2.0.js
@@ -1,131 +1,94 @@
-var check;
-var x;
-var time = 5000;
-var stop;
-var pass = true;
-var active = false;
-var notif = true;
-var div = document.createElement("DIV");
-div.id = "main";
-div.style.height = "50px";
-div.style.width = "500px";
-div.style.margin = "0px auto 0px auto";
-div.style.border = "3px solid black";
-div.style.background = "gray";
-div.style.color = "black";
-document.body.appendChild(div);
-var toggle = document.createElement("BUTTON");
-toggle.id = "toggle";
-toggle.setAttribute('onclick', "start()");
-//toggle.style.width = "50px";
-toggle.style.height = "25px";
-toggle.style.border = "3px solid black";
-toggle.style.margin = "auto";
-toggle.innerHTML = "Bot: Off";
-toggle.style.background = "white";
-document.getElementById("main").appendChild(toggle);
-var skip = document.createElement("BUTTON");
-skip.id = "skip";
-skip.setAttribute("onclick", "nope()");
-//skip.style.width = "50px";
-skip.style.height = "25px";
-skip.style.border = "3px solid black";
-skip.style.margin = "auto";
-skip.innerHTML = "Skip new words: On";
-skip.style.background = "white";
-document.getElementById("main").appendChild(skip);
-var alert = document.createElement("BUTTON");
-alert.id = "al";
-alert.setAttribute("onclick", "notify()");
-//skip.style.width = "50px";
-alert.style.height = "25px";
-alert.style.border = "3px solid black";
-alert.style.margin = "auto";
-alert.innerHTML = "Alert new words: On";
-alert.style.background = "white";
-document.getElementById("main").appendChild(alert);
-function start() {
-	if(active == false) {
-		document.getElementById("toggle").innerHTML = "Bot: On";
-		set("norm");
-		active = true;
-	} else if (active == true) {
-		document.getElementById("toggle").innerHTML = "Bot: Off";
-		set("stop");
-		active = false
-	}
+
+var interval = {
+	"test": "",
+	"click": "",
+	"deltest": "",
+} 
+var bool = {
+	"bot": false,
+	"on": false,
+} 
+var object = {
+	"div": "",
+	"button1": "",
 }
+
+object.div = document.createElement("DIV");
+object.div.style.width = "500px";
+object.div.style.height = "50px";
+object.div.style.border = "3px solid black";
+object.div.id = "div";
+object.div.style.background = "gray";
+
+object.button1 = document.createElement("BUTTON");
+object.button1.style.width = "100px";
+object.button1.style.height = "25px";
+object.button1.style.border = "3px solid black";
+object.button1.style.background = "white";
+object.button1.id = "button1";
+object.button1.innerHTML = "Off";
+object.button1.setAttribute("onclick", "start()");
+
+
+document.body.appendChild(object.div);
+document.getElementById("div").appendChild(object.button1);
+
 
 function test() {
-	if(document.Pass) {
-		set("clear");
-		console.log("Pass element exists, clicking");
-		set("click");
-	} else if (document.getElementById("next-btn")) {
-		if(pass == true) {
-			set("clear");
-		x = document.querySelectorAll("#choice-section li");
-		console.log("Next button");
-		x[0].click();
-		x[1].click();
-		x[2].click();
-		document.getElementById("next-btn").click();
-		set("delay");
+	if(bool.bot == true) {
+		if(document.Pass) {
+			bool.bot = false;
+			console.log("Pass element found");
+			set("click");
+		} else if (document.getElementById("next-btn")) {
+			set("stop");
+			let x = document.querySelectorAll("#choice-section li");
+			console.log("next button");
+			x[0].click();
+			x[1].click();
+			x[2].click();
+			document.getElementById("next-btn").click();
+			set("test");
 		} else {
-			/*if(alert == true) {
-			
-			}*/
+			console.log("No elements found");
 		}
-		
-	} else if (document.getElementById("answer-bot")) {//move this before pass element
-		click();
-	} else {
-		console.log("No recognized elements");
 	}
-} function click() {
-console.log("clicked");
-	document.Pass.click();
-	set("delay");
-} 
-
-function set(type) {
-	if(type == "norm") {
-	clearInterval(stop);
-	check = setInterval(test, 1);
-	} else if(type == "delay") {
-		setTimeout(function del() {check = setInterval(test, 1);}, 1000);
-	} else if(type == "clear") {
-		clearInterval(check);
-	} else if(type == "click") {
-		let rand = Math.floor(Math.random() * 31) + 1; //something is wrong with this--------------------
+} function set(type) {
+	if(type == "click") {//waits time to click
+		let rand = Math.floor(Math.random() * 31) + 1;
 		rand = rand * 1000;
 		console.log(rand);
-		setTimeout(click, rand);
-	} else if (type == "stop") {
-		stop = setInterval(stop, 1);
+		interval.click = setTimeout(press, 5000);
+	
+	} else if (type == "test") {//starts testing after 1 second
+		interval.deltest = setTimeout(function x(){bool.bot = true;}, 1000);
+	} else if (type == "stop") { //CLEAR ALL TIMEOUTS AND INTERVALS
+		clearTimeout(interval.click);
+		clearTimeout(interval.deltest);
+		bool.bot = false; //may cause a problem
 	}
-} function stop() {
-	set("clear");
-} function nope() {
-	if(pass == true) {
-		document.getElementById("skip").innerHTML = "Skip new words: Off";
-		pass = false;
-		console.log("No longer skipping new words");
-	} else if (pass == false) {
-		document.getElementById("skip").innerHTML = "Skip new words: On";
-		pass = true;
-		console.log("Skipping new words");
-	}
-} function notify() {
-	if(notif == true) {
-		document.getElementById("al").innerHTML = "Alert new words: Off";
-		notif = false;
-		console.log("Nofifications are inactive");
-	} else if(notif == false) {
-		document.getElementById("al").innerHTML = "Alert new words: On";
-		notif = true;
-		console.log("Nofifications are active");
+} function press() {
+	document.Pass.click();
+	console.log("Clicked");
+	set("test");
+	
+} 
+
+
+function start() {
+	if(bool.on == true) {
+		set("stop");
+		bool.on = false;
+		bool.bot = false;
+		set("stop");
+		console.log("Bot Inactive");
+		document.getElementById("button1").innerHTML = "Off";
+	} else if (bool.on == false) {
+		bool.on = true;
+		bool.bot = true;
+		console.log("Bot Active");
+		document.getElementById("button1").innerHTML = "On";
 	}
 }
 
-
+interval.test = setInterval(test, 1);
